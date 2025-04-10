3220c7144846a4ebb6c5f51bea32ef765dd0c68a
Yunes Ahmed <42625722+yunisradman@users.noreply.github.com>
2025-04-10 10:33:11 +0300

    Add files via upload

---
+++ js/script-nav.js
@@ -0,0 +1,6 @@
+const navbarToggle=document.querySelector('.navbar-toggle');
+const navbarMenu=document.querySelector('.navbar-menu');
+navbarToggle.addEventListener('click',()=>{
+     navbarToggle.classList.toggle('active');
+     navbarMenu.classList.toggle('active');
+});
